c1bc5096ed0914ce928806b6eb98f7d30a88408c
Rori Hoover <135872533+Rlhoove1@users.noreply.github.com>
2026-03-03 10:52:19 -0700

    Add comment
    Added a comment about the ongoing work for fetching all project data.

--- get_project_data.py
+++ get_project_data.py
@@ -9,3 +9,5 @@ response = requests.get(url, auth=(uid, pw),params={"page": 1, "page_size": 500}
 responselist = json.loads(response.text)
 df = pd.json_normalize(responselist['projects'])
 df
+
+#work in progress for how exactly to get *all* the project data on raverly with out the site freaking out 
