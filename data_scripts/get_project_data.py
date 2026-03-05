9129f8e3b389acc33b9e57811856c638608910ed
Rlhoove1 <rlhoove1@asu.edu>
2026-03-05 12:52:48 -0700

    handling server timeout

--- data_scripts/get_project_data.py
+++ data_scripts/get_project_data.py
@@ -1,26 +1,35 @@
 import requests
 import json
 import pandas as pd
+import time
+
+
 
 url = "https://api.ravelry.com/projects/search.json"
 # use ur acess key, define in console so it doenst end up on github 
 uid, pw = "read-9add40de0449b103573f7fc8b71c002d", "AmETw80XYOTeWthpmGBbcmSqjCcVHwnq3dvvkVYi"
 
-
-
-url = "https://api.ravelry.com/projects/search.json"
-uid, pw = "read-9add40de0449b103573f7fc8b71c002d", "AmETw80XYOTeWthpmGBbcmSqjCcVHwnq3dvvkVYi"
-
 # initialize dataframe and starting page
-df = pd.DataFrame()   
-n = 1
+response = requests.get(url, auth=(uid, pw),params={ "page": 1, "page_size": 500} )
+responselist = json.loads(response.text)
+#print(response.text)url = "https://api.ravelry.com/projects/search.json"
+df = pd.json_normalize(responselist["projects"])
+
+n = 2
+start = time.time()
 
 while True:
-    response = requests.get(
-        url,
-        auth=(uid, pw),
-        params={"page": n, "page_size": 500}
-    )
+    try:
+        response = requests.get(
+            url,
+            auth=(uid, pw),
+            params={"page": n, "page_size": 500},
+            timeout=(3, 10)
+        )
+    except requests.exceptions.RequestException as e:
+        print("Connection error:", e)
+        time.sleep(30)
+        continue
 
     responselist = json.loads(response.text)
 
@@ -29,17 +38,21 @@ while True:
         break
 
     tmp = pd.json_normalize(responselist['projects'])
-
-    df = pd.concat([df, tmp], ignore_index=True)
-
+   
+    if df.empty:
+        df = tmp
+    else:
+        df = pd.concat([df, tmp], ignore_index=True)
     # proof of life
     if n % 10 == 0:
         print(f"Page {n} collected, rows so far: {len(df)}")
-    if n == 20:
+    if n == 100:
         break
     n += 1
 
+end = time.time()
+print("runtime:", end - start)
 
-df.to_csv("full_project_data.csv", index=False)
+df.to_csv("project_data.csv", index=False)
 
 #work in progress for how exactly to get *all* the project data on raverly with out the site freaking out 
