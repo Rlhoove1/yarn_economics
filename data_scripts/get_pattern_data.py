5e00448d792a89912ea16503318f754f1ca448f1
Rori Hoover <135872533+Rlhoove1@users.noreply.github.com>
2026-03-15 20:24:39 -0700

    Update and rename get_pattern_data.py to deleteme.md

--- data_scripts/get_pattern_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import pandas as pd
-url = "https://api.ravelry.com/patterns/search.json"
-uid, pw = "", ""
-response = requests.get(url, auth=(uid, pw),params={"page": 2, "page_size": 100} )
-responselist = json.loads(response.text)
-df = pd.json_normalize(responselist['patterns'])
-df
